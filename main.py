d3a01c6a36c9ce4e6431ead292c78837150f9355
Nasya <sadykovaanastasia@yandex.ru>
2019-02-12 16:14:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import sys
-from Project import *
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class MyWin(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+    def __init__(self):
+        QMainWindow.__init__(self)
+        loadUi('Project.ui',self)
+        
 
-        self.ui.pushButton.clicked.connect(self.MyFunction)
-
-    def MyFunction(self):
-        self.ui.textEdit_2.setText("")
-        strings=self.ui.textEdit.toPlainText()
-        massive=strings.split('\n')
+    @pyqtSlot()
+    def on_pushButton_clicked(self):
+        self.textEdit_2.clear()
+        massive=self.textEdit.toPlainText().split('\n')
         result=''
         letters=['а','е','ё','у','ы','и','о','ю','я','э']
         for string in massive:
             count=0
             for i in string.lower():
                 for lett in letters:
-                    if (i==lett): count=count+1
+                    if (i==lett): count+=1
             if(count>0):
                 result=result+string+' - '+str(count)+' слогов'+'\n'
             else:
                 result=result+'\n'
-        self.ui.textEdit_2.setText(result)
+        self.textEdit_2.setText(result)
 
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
